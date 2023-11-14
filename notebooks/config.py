dc6767e91f309ad0f88ffe28b35731b6ff6e4434
marcopellegrinoit <marcopellegrino.it@gmail.com>
2023-11-14 10:06:51 +0100

    add configs

---
+++ notebooks/config.py
@@ -0,0 +1,13 @@
+# Configurations
+FEATURE_GROUP_NAME = 'weather_fg'
+FEATURE_VIEW_NAME = 'weather_fv'
+MODEL_NAME = 'weather_code_xgboost_model'
+MODEL_METRIC = 'MSE'
+OPTIMIZE_DIRECTION = 'min'
+MODEL_PATH = 'weather_code_model'
+BASELINE_URL_OPEN_METEO = 'https://api.open-meteo.com/v1/forecast'
+TIMEZONE = 'Europe/Berlin'
+
+# Utrecht coordinates
+LATITUDE = '52.0908'
+LONGITUDE = '5.1222'
